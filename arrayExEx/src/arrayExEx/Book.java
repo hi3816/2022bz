f3f0b736e290268685dc08aac5d27429fcf699af
hi3816 <wlgp2534@naver.com>
2022-05-19 13:09:57 +0900

    [추가] : 2차원배열 문제

---
+++ arrayExEx/src/arrayExEx/Book.java
@@ -0,0 +1,31 @@
+package arrayExEx;
+
+public class Book {
+	private String author;
+	private String bookName ;
+	
+	public Book (String author, String bookName) {
+		this.author = author;
+		this.bookName = bookName;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	public String getAuthor() {
+		return this.author;
+	}
+	public String getBookName() {
+		return this.bookName;
+	}
+	
+	public void showInfo() {
+		System.out.println("작가 : "+author);
+		System.out.println("책이름 : "+bookName);
+	}
+	
+}
---
+++ arrayExEx/src/arrayExEx/main.java
@@ -0,0 +1,62 @@
+package arrayExEx;
+
+public class main {
+
+
+	public static void main(String[] args) {
+		/*
+		Book book1 = new Book("저자1","책1");
+		Book book2 = new Book("저자2","책2");
+		book2.showInfo();
+		book2 = book1;
+		book2.showInfo();
+		book1.setAuthor("다른저자1");
+		book1.setBookName("다른책1");
+		book2.showInfo();
+		*/
+		
+		/*
+		int a =5;
+		int b =6;
+		
+		System.out.println("a의 값은 : "+a);
+		System.out.println("b의 값은 : "+b);
+		
+		b = a;
+		
+		System.out.println("a의 값은 : "+a);
+		System.out.println("b의 값은 : "+b);
+	*/
+		
+		/*
+		Book book1 = new Book("책1","저자1");
+		Book book2 = new Book("책2","저자2");
+		
+		System.out.println(book1.hashCode());
+		System.out.println(book2.hashCode());
+		
+		book2.setAuthor(book1.getAuthor());
+		book2.setBookName(book1.getBookName());
+		
+		book2.showInfo();
+		*/
+		
+		Book[] library1 = new Book[5];
+		
+		library1[0] = new Book("1저자1","1책1");
+		library1[1] = new Book("1저자2","1책2");
+		library1[2] = new Book("1저자3","1책3");
+		library1[3] = new Book("1저자4","1책4");
+		library1[4] = new Book("1저자5","1책5");
+		
+		Book[] library2 = new Book[5];
+		
+		System.arraycopy
+		
+		
+		
+		
+		
+	}
+
+}
