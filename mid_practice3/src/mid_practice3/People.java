91e88f58475e87eefff79e26b280b4cb7811d1f9
hi3816 <wlgp2534@naver.com>
2022-05-21 23:16:37 +0900

    [추가] : 중간실습3-3

---
+++ mid_practice3/src/mid_practice3/People.java
@@ -0,0 +1,13 @@
+package mid_practice3;
+
+public class People {
+	String name;
+	double score;
+	public People (String name, double score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	public String getName() {return name;}
+	public double getScore() {return score;}
+}
